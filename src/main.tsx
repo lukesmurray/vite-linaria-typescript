8745bda26ed146db99cf732fc5e2438a553da6c9
Luke Murray <lukepigeonmail@gmail.com>
2022-08-09 11:28:34 -0400

    basic linaria stuff working

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { styled } from "@linaria/react";
+import tw, { css, styled, theme } from "twin.macro";
 
-const Container = styled.div`
-  font-size: 24px;
+function LinariaCssClassName() {
+  return (
+    <h1
+      className={css`
+        font-size: 2rem;
+        background: #73bfdd9d;
+        color: #0519f6;
+      `}
+    >
+      Linaria CSS Classname
+    </h1>
+  );
+}
+
+const LinariaStylesAsAStyledTag = styled.h1`
+  font-size: 2rem;
+  background: #73bfdd9d;
+  color: #0519f6;
 `;
 
+const stylesObject = {
+  fontSize: "2rem",
+  background: "#73bfdd9d",
+  color: "#0519f6",
+};
+
+const LinariaObjectInterpolationInStyled = styled.h1`
+  ${stylesObject}
+`;
+
+function LinearObjectInterpolationInCssClassName() {
+  return (
+    <h1
+      className={css`
+        ${stylesObject}
+      `}
+    >
+      Linaria Object Interpolation in CSS Classname
+    </h1>
+  );
+}
+
 function App() {
-  return <Container>This is my app</Container>;
+  return (
+    <>
+      <LinariaCssClassName />
+      <LinariaStylesAsAStyledTag>
+        Linaria Styles As A Styled Tag
+      </LinariaStylesAsAStyledTag>
+      <LinariaObjectInterpolationInStyled>
+        Linaria Object interpolation in Styled
+      </LinariaObjectInterpolationInStyled>
+      <LinearObjectInterpolationInCssClassName />
+    </>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
